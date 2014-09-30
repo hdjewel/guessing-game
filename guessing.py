b44e0c9e7412b843307d95c6fc9dd04c8043ca65
Hackbright Student <info@hackbrightacademy.com>
2014-09-30 16:20:49 -0700

    The program now generates a random number

--- guessing.py
+++ guessing.py
@@ -1,2 +1,12 @@
+import random
+
+# get the player's name from the user
 username = raw_input("Please enter your name: ")
+# print the greeting
 print ("Hello guessing game player %s.  It's nice to meet you.") % username
+
+# generate a random number for the player to guess
+random_number =  random.randrange(1,101)
+
+
+
